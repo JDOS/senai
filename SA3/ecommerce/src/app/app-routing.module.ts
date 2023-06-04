d603071f40b45bda98e1a7a98404fa3f28192508
Jonatas <jonatasdos@outlook.com>
2023-06-04 14:08:56 -0300

    Angular Site

--- SA3/ecommerce/src/app/app-routing.module.ts
+++ SA3/ecommerce/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { SuporteComponent } from './views/suporte/suporte.component';
 
-const routes: Routes = [];
+// rotas para as paginas do site
+const routes: Routes = [
+  {path: "", component:HomeComponent},
+  {path: "login", component:LoginComponent},
+  {path: "suporte", component:SuporteComponent}  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- SA3/ecommerce/src/app/app.module.ts
+++ SA3/ecommerce/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { SuporteComponent } from './views/suporte/suporte.component';
+import { LojaComponent } from './views/loja/loja.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SuporteComponent,
+    LojaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
