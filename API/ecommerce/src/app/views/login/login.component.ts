bb4c73f38fd9bb8b95358f9cdfe1e446f97931a1
Jonatas <jonatasdos@outlook.com>
2023-06-04 22:43:01 -0300

    Segundo encontro screenshots

--- API/ecommerce/src/app/views/login/login.component.ts
+++ API/ecommerce/src/app/views/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { User } from 'src/app/model/user';
 import { LoginService } from 'src/app/services/login.service';
+// import para redirecionamento
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,16 +11,25 @@ import { LoginService } from 'src/app/services/login.service';
 })
 export class LoginComponent {
   
-  constructor(private loginService:LoginService){}  
+  constructor(private loginService:LoginService, private router:Router){}  
   
   userModel = new User();
+
+  mensagem = ""
   
   receberDados(){
     console.log(this.userModel)
 
 
     this.loginService.login(this.userModel).subscribe((response) => {
-      console.log(response.body.user.nome)
-    })
+      console.log("Sucesso!")
+      localStorage.setItem("nomeUsuario", response.body.user.nome)
+      this.router.navigateByUrl("/")  
+    
+    }, (erro) => {
+      console.log("ERROR")
+      // alert(erro.error)
+      this.mensagem=erro.error
+    })  
   }
 }
