ca9723f36aadbf47ffd1ace3130781ca66b0528e
Jonatas <jonatasdos@outlook.com>
2023-06-07 07:47:00 -0300

    encontro 7 e 8

---
+++ backend/encontro_7_e_8/projeto/Classes/Pessoa.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using projeto.Interfaces;
+
+
+namespace projeto.Classes
+{
+
+    public abstract class Pessoa :  IPessoa
+    {
+        public string? nome{get;set;}
+        public Endereco? endereco {get;set;}
+        public float? rendimento{get;set;}
+
+        public abstract float CalcularImposto(float rendimento);
+    }
+}
