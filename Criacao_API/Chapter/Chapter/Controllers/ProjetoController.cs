df2209de48491f46428d27792b127f7a01983733
Jonatas <jonatasdos@outlook.com>
2023-06-09 08:14:37 -0300

    Desenvolvimento API

---
+++ Criacao_API/Chapter/Chapter/Controllers/ProjetoController.cs
@@ -0,0 +1,33 @@
+﻿using Chapter.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter.Controllers
+{
+    //saida do JSON
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjetoController : ControllerBase
+    {
+        private readonly ProjetoRepository? _projetoRepository;
+        public ProjetoController(ProjetoRepository projetoRepository)
+        {
+            _projetoRepository = projetoRepository;
+        }
+
+        [HttpGet]
+        public IActionResult listar()
+        {
+            try
+            {
+                return Ok(_projetoRepository.Listar());
+            }
+            catch (Exception ex)
+            {
+                //return BadRequest(ex.Message);
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ Criacao_API/Chapter/Chapter/Repositories/ProjetoRepository.cs
@@ -0,0 +1,21 @@
+﻿using Chapter.Contexts;
+using Chapter.Models;
+using Microsoft.Identity.Client;
+
+namespace Chapter.Repositories
+{
+    public class ProjetoRepository
+    {
+        private readonly Sqlcontext _context;
+
+        public ProjetoRepository(Sqlcontext context) {
+            //
+            _context = context;
+
+        }
+        public List<Projeto> Listar()
+        {
+            return _context.Projetos.ToList();
+        }
+    }
+}
