12ffd94c53eb403ed72b24a8ef848aa7f11c98fa
Jonatas <jonatasdos@outlook.com>
2023-06-10 09:21:21 -0300

    CRUD API

--- Criacao_API/Chapter/Chapter/Controllers/ProjetoController.cs
+++ Criacao_API/Chapter/Chapter/Controllers/ProjetoController.cs
@@ -1,4 +1,5 @@
-﻿using Chapter.Repositories;
+﻿using Chapter.Models;
+using Chapter.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -29,5 +30,72 @@ namespace Chapter.Controllers
                 throw new Exception(ex.Message);
             }
         }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Projeto projetobuscado = _projetoRepository.BuscarPorId(id);
+                if(projetobuscado==null){
+                    return NotFound();
+                }
+
+                else {
+                    return Ok(_projetoRepository.BuscarPorId(id));
+                }
+            }
+            catch (Exception ex)
+            {
+                //return BadRequest(ex.Message);
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Projeto p)
+        {
+            try
+            {
+                _projetoRepository.Cadastrar(p);
+                return StatusCode(201);
+            }
+            catch (Exception ex)
+            {
+                //return BadRequest(ex.Message);
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                 _projetoRepository.Deletar(id);
+                return Ok("Projeto Removido com Sucesso");
+            }
+            catch (Exception ex)
+            {
+                //return BadRequest(ex.Message);
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Alterar(int id, Projeto p)
+        {
+            try
+            {
+                _projetoRepository.Alterar(id, p);
+                return Ok("Projeto alterado com Sucesso");
+            }
+            catch (Exception ex)
+            {
+                //return BadRequest(ex.Message);
+                throw new Exception(ex.Message);
+            }
+        }
+
     }
 }
--- Criacao_API/Chapter/Chapter/Repositories/ProjetoRepository.cs
+++ Criacao_API/Chapter/Chapter/Repositories/ProjetoRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Chapter.Contexts;
 using Chapter.Models;
 using Microsoft.Identity.Client;
+using System.Runtime.CompilerServices;
 
 namespace Chapter.Repositories
 {
@@ -16,6 +17,37 @@ namespace Chapter.Repositories
         public List<Projeto> Listar()
         {
             return _context.Projetos.ToList();
+        }
+
+        public Projeto BuscarPorId(int id)
+        {
+            return _context.Projetos.Find(id);
+        }
+
+        public void Cadastrar(Projeto p)
+        {
+            _context.Projetos.Add(p);
+            _context.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+           Projeto p = _context.Projetos.Find(id);
+           _context.Projetos.Remove(p);
+           _context.SaveChanges();  
+        }
+
+        public void Alterar (int id, Projeto p)
+        {
+            Projeto projetobuscado = _context.Projetos.Find (id);
+            if (projetobuscado != null)
+            {
+               projetobuscado.NomeDoProjeto = p.NomeDoProjeto;
+               projetobuscado.Area = p.Area;
+               projetobuscado.Status = p.Status;
+               _context.Projetos.Update(projetobuscado);
+               _context.SaveChanges();
+            }
         }
     }
 }
